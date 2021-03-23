029cefbec4ef6756d5a6dce68b26c97a8a3260dc
R-Niagra <rizwannagra@live.com>
2021-03-23 19:52:23 +0500

    adding template file

---
+++ template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> ipair;
+typedef pair<ll, ll> lpair;
+
+#define p1(a) cout << a << "\n"
+#define p2(a, b) cout << a << " " << b << "\n"
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define FR(i, a) for (int i = 0; i < a; i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define FRd(i, a) for (int i = (a)-1; i >= 0; i--)
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+
+// const ll INF = 1e18;
+
+void solveCase()
+{
+    //    int n,a,b;
+    //    cin>>n;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cout << "Case #" << i + 1 << ": ";
+        solveCase();
+    }
+    return 0;
+}
